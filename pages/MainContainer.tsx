91cd6f2a5b2c0d9f795f77bcfc44d81bd8df9399
sasankthapa <thapas@beloit.edu>
2021-10-11 11:24:10 -0500

    Some Styling changes

--- pages/MainContainer.tsx
+++ pages/MainContainer.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 import profileImage from '../public/static/images/profile.jpg'
 
 const MainContainer:React.FC<{}>=()=>{
-    return <div className="relative flex items-start items-center justify-center w-screen h-screen bg-gray-300">
+    return <div className="relative flex box-content items-start items-center justify-center w-screen h-screen bg-gray-300">
         <Transition
           as={Fragment}
           appear={true}
@@ -24,13 +24,14 @@ const MainContainer:React.FC<{}>=()=>{
                 <h1 className="relative mb-5 text-3xl text-center border-b-2 border-black lg:text-3xl">
                     Sashank Thapa
                 </h1>
-                <div className="w-full p-2 mb-3 rounded-lg cursor-pointer hover:bg-green-400 transition-colors shadow-sm bg-blue-50">
-                    <Link href="me"><a>sasank.codes/work</a></Link>
-                </div>
+                <Link href="me">
+                    <button className="w-full p-2 mb-3 rounded-lg cursor-pointer hover:bg-green-400 transition-colors shadow-sm bg-blue-50">
+                        <a>sasank.codes/work</a>
+                    </button>
+                </Link>
                 <div className="flex flex-row items-center justify-center w-full mt-2 gap-10 md:gap-3">
                     <Link href="https://sasank.codes/assets/resume.pdf"><a target="_blank" rel="noreferrer"><TiDocumentText className="m-1 text-xl lg:text-2xl"/></a></Link>
                     <Link href="https://github.com/sasankthapa"><a target="_blank" rel="noreferrer"><FaGithub className="m-1 text-xl lg:text-2xl"/></a></Link>
-                    <Link href="https://leetcode.com/sasankthapa"><a target="_blank" rel="noreferrer"><SiLeetcode className="m-1 text-xl lg:text-2xl"/></a></Link>
                     <Link href="https://linkedin.com/in/sasank-t-b815b1104/"><a target="_blank" rel="noreferrer"><FaLinkedin className="m-1 text-xl lg:text-2xl"/></a></Link>
                 </div>
             </div>
