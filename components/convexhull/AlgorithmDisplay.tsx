9dbfb26adc383e19e7ff371a9d704b1ef0d582b9
sasankthapa <thapas@beloit.edu>
2021-10-20 09:01:31 -0500

    Some styling changes

--- components/convexhull/AlgorithmDisplay.tsx
+++ components/convexhull/AlgorithmDisplay.tsx
@@ -4,10 +4,12 @@ import { AlgorithmDisplayProps } from '../../types/convexhull/grahamscan.types'
 const AlgorithmDisplay: React.FC<AlgorithmDisplayProps> = ({ playing, algoName, pointsNum, setPointsNum,  render, steps, currStep, currPlaneSize, sparseRadius, setPlaneSize, setSparseRadius, step, play, pause }) => {
     console.log(currStep)
     return <div className="flex flex-col p-2 text-white bg-blue-500 md:w-3/5">
-        <div className="block ">
-            <h1 className="font-bold">{algoName}</h1>
+        <div className="block mb-2 border-b-2">
+            <h1 className="font-bold border-b-2 border-white text-lg mb-1">{algoName}</h1>
+            <p>Graham scan is a fast algorithm taking O(nlogn) time to calculate the <a href={"https://en.wikipedia.org/wiki/Convex_hull"}><span className="italic no-underline font-bold text-current">convex hull</span></a> of a set of points.
+            </p>
         </div>
-        <div className="flex w-full items-center gap-1 py-2 bg-blue-500 my-1 border-t-2">
+        <div className="flex w-full items-center gap-1 py-2 bg-blue-500 my-1">
             <div className="flex-grow text-center">
                 <input onChange={(e)=>setPlaneSize(e.target.valueAsNumber)} id="planesize" className="w-full text-black text-center" type="number" value={currPlaneSize} placeholder="Plane Size" />
                 <label htmlFor="planesize">Plane Size</label>
@@ -24,10 +26,10 @@ const AlgorithmDisplay: React.FC<AlgorithmDisplayProps> = ({ playing, algoName, 
                 Render
             </button>
         </div>
-        <div className="flex justify-center w-full gap-2">
-            <button onClick={step} className="p-2 bg-gray-100 text-gray-700 rounded-lg">Step</button>
-            <button onClick={play} className={`p-2 text-gray-700 rounded-lg ${playing?"bg-green-500":"bg-green-200"}`}>Play</button>
-            <button onClick={pause} className="p-2 bg-gray-100 text-gray-700 rounded-lg">Pause</button>
+        <div className="flex justify-center w-full">
+            <button onClick={step} className="mx-2 p-2 bg-gray-100 text-gray-700 rounded-lg">Step</button>
+            <button onClick={play} className={`mx-2 p-2 text-gray-700 rounded-lg ${playing?"bg-green-500":"bg-green-200"}`}>Play</button>
+            <button onClick={pause} className="mx-2 p-2 bg-gray-100 text-gray-700 rounded-lg">Pause</button>
         </div>
         <div className="my-2 p-2 bg-gray-200 text-black rounded-md border-2 border-black box-border">
             <div>
--- pages/MainContainer.tsx
+++ pages/MainContainer.tsx
@@ -27,18 +27,18 @@ const MainContainer:React.FC<{}>=()=>{
             >
                 <div className="select-none pointer-events-auto relative px-5 py-10 text-center bg-green-300 shadow-2xl md:pt-16 rounded-2xl">
                     <div className="absolute top-0 w-20 h-20 overflow-hidden border-2 border-gray-100 rounded-full shadow-xl transform -translate-x-1/2 -translate-y-1/2 left-1/2 sm:border-6">
-                        <Image className="filter brightness-100" src={profileImage} layout="responsive" alt="The creator Sasank" />
+                        <Image className="filter brightness-125" src={profileImage} layout="responsive" alt="The creator Sasank" />
                     </div>
                     <h1 className="relative mb-5 text-3xl text-center border-b-2 border-black lg:text-3xl">
                         Sashank Thapa
                     </h1>
                     <Link href="/me" passHref>
-                        <button className="w-full hover:bg-green-400 p-2 mb-3 rounded-lg cursor-pointer transition-colors shadow-sm bg-blue-50">
+                        <button className="w-full hover:bg-purple-200 p-2 mb-3 rounded-lg cursor-pointer transition-colors shadow-sm bg-blue-50">
                             <a>/me</a>
                         </button>
                     </Link>   
                     <Link href="/convexhull" passHref>    
-                        <button className="w-full p-2 mb-3 rounded-lg cursor-pointer hover:bg-green-400 transition-colors shadow-sm bg-blue-50">
+                        <button className="w-full p-2 mb-3 rounded-lg cursor-pointer hover:bg-purple-200 transition-colors shadow-sm bg-blue-50">
                             <a>/convexhull</a>
                         </button>
                     </Link>   
